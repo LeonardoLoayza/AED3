1472febe6f54c95e20c6c71d8e4d10bae443f556
leonardo <leonardoloayza28@gmail.com>
2026-09-07 00:34:46 -0500

    fl

---
+++ forwardlist.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class CNode{
+public: 
+    int v;
+    CNode * next; 
+    CNode(int _v){
+        v=_v;
+        next=nullptr;
+    }
+};
+    
+class ForwardList{
+public:     
+    CNode * head;
+    int nelem;
+    ForwardList(){
+        head=nullptr;
+        nelem=0;
+    }
+
+    void pushback(int x){
+        CNode**p=&head;
+        while(*p){
+            p=&((*p)->next);
+        }
+        *p=new CNode(x);
+        nelem++;
+        return; 
+    }
+
+    void pushfront(int x){
+        CNode*n=new CNode(x);
+        n->next=head;
+        head=n; 
+        nelem++;
+        return;
+    }
+
+    void popback(){
+        CNode**p=&head;
+        while(*p&&(*p)->next){
+            p=&((*p)->next);
+        }
+        delete *p;
+        *p=nullptr;
+        nelem--;
+    }
+
+    void popfront(){
+        if(head==nullptr) return;
+        CNode*n=head;
+        head=head->next;
+        delete n;
+        n=0;
+        nelem--;
+    }
+
+};
+
+int main(){
+
+}
