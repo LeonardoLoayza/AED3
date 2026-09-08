9407c81c3b6eba0b6dd3066976faf5ec97f23aea
leonardo <leonardoloayza28@gmail.com>
2026-09-07 21:22:56 -0500

    eraseAdyacentDuplicates

--- forwardlist.cpp
+++ forwardlist.cpp
@@ -68,9 +68,66 @@ public:
         return p->v;
     }
 
+    void print(){
+        for(CNode *p = head; p != nullptr; p = p->next)
+            cout << p->v << " ";
+        cout << endl; 
+    }
+
+    // collapse 
+    void collapse(){
+
+    }
+
+    void eraseAdyacentDuplicates(){
+        CNode**p=&head;
+        CNode*veci=nullptr;
+        while(*p && (*p)->next){
+            veci=(*p)->next;
+            if ((*p)->v==veci->v){
+                (*p)->next=veci->next;
+                delete veci;
+                nelem--;
+            }
+            else {
+                p=&((*p)->next);
+            }
+
+        }
+    }
 
 };
 
 int main(){
+    ForwardList lista;
+    // Insertamos: 1, 1, 1, 2, 3, 3, 4, 1
+    lista.pushback(1);
+    lista.pushback(1);
+    lista.pushback(1);
+    lista.pushback(2);
+    lista.pushback(3);
+    lista.pushback(3);
+    lista.pushback(4);
+    lista.pushback(1); // Este 1 se queda porque no es adyacente a los primeros
+    cout << "Tamanio original: " << lista.nelem << endl;
+    lista.eraseAdyacentDuplicates();
+    cout << "Tamanio tras eraseDuplicates: " << lista.nelem << endl;
+    // Imprimimos la lista
+    cout << "Lista final: ";
+    for(int i = 0; i < lista.nelem; i++){
+        cout << lista[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+
+
+    // ForwardList f;
+    // f.pushback(5); f.print(); 
+    // f.pushback(5); f.print(); 
+    // f.pushback(4); f.print(); 
+    // f.pushfront(1); f.print(); 
+    // f.popfront(); f.print(); 
+
 
 }
