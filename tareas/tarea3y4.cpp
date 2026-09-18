c750f1bcc2e69b37a0ec9aa96b2965a1aaabf6aa
Leonardo André Loayza Paucar <112676148+LeonardoLoayza@users.noreply.github.com>
2026-09-18 13:23:10 -0500

    tarea polaca

--- tareas/tarea3y4.cpp
+++ tareas/tarea3y4.cpp
@@ -7,169 +7,219 @@
 // usar una pila o una cola. 
 // multiplicar 27 por cualquier numero y dado eso
 // tratar de adivinar el ultimo digito. 
-
 #include <iostream>
 #include <vector>
 #include <cctype>
-#include <string> 
+#include <string>
+#include <stdexcept>
 
 using namespace std;
 
-template <class T>
-class Asc {
-public:
-	bool operator()(T a, T b) {
-		return a < b;
-	}
-};
-
-template<class T, int N=100> 
+template<class T, int N = 100>
 class Pila {
 private:
-    T arr[N]; 
-    int elem=0;
-public: 
-    bool empty(){return elem==0;}
-    bool full(){return elem==N;}
+    T arr[N];
+    int elem = 0;
 
-    bool push(T e){
-        if(full()) return false;
-        arr[elem]=e;
+public:
+    bool empty() {
+        return elem == 0;
+    }
+
+    bool full() {
+        return elem == N;
+    }
+
+    bool push(T e) {
+        if (full()) return false;
+
+        arr[elem] = e;
         elem++;
+
         return true;
     }
 
-    bool pop(){
-        if(empty())return false;
+    bool pop() {
+        if (empty()) return false;
+
         elem--;
+
         return true;
     }
 
-    T top(){
-        if(!empty()){ return arr[elem-1]; }
-        throw runtime_error("vaciooo");
+    T top() {
+        if (!empty()) {
+            return arr[elem - 1];
+        }
+
+        throw runtime_error("Pila vacia");
     }
 };
 
-int precedencia(char op){
-    if (op=='+'||op=='-') return 1;
-    if (op=='*'||op=='/') return 2;
-    if (op=='^') return 3;
-    return 0; 
+int precedencia(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    if (op == '^') return 3;
+
+    return 0;
 }
 
-string infijaPostfija(string infija){
+string infijaPostfija(string infija) {
+
     Pila<char> operadores;
-    string postfija="";
-    string tempNum="";
 
-    for(int i=0;i<infija.length();i++){
-        char actual=infija[i];
+    string postfija = "";
+    string tempNum = "";
 
-        if (isdigit(actual)||actual=='.'){
-            tempNum+=actual;
+    for (int i = 0; i < infija.length(); i++) {
+
+        char actual = infija[i];
+
+        if (isdigit(actual) || actual == '.') {
+
+            tempNum += actual;
         }
-        else{
-            // se lee algo que no es numero, el numero ya acabó
-            if(tempNum!=""){
-                postfija+=tempNum+" ";
-                tempNum=" ";
+
+        else {
+
+            if (tempNum != "") {
+
+                postfija += tempNum + " ";
+
+                tempNum = "";
             }
 
-            if(actual==' '){
+            if (actual == ' ') {
                 continue;
             }
 
-            if(actual=='('){
+            if (actual == '(') {
+
                 operadores.push(actual);
             }
 
-            else if(actual==')'){
-                while(!operadores.empty() && operadores.top() != '('){
-                    postfija+=operadores.top();
-                    postfija+=" ";
-                    operadores.pop(); 
-                }
-                if(!operadores.empty()&&operadores.top()!='('){
-                    operadores.pop(); 
-                }
-            }
+            else if (actual == ')') {
 
-            else if (actual == '+' || actual == '-' || actual == '*' || actual == '/') {
-                while (!operadores.empty() && operadores.top() != '(' && 
-                       precedencia(operadores.top()) >= precedencia(actual)) {
+                while (!operadores.empty() &&
+                    operadores.top() != '(') {
+
                     postfija += operadores.top();
                     postfija += " ";
+
                     operadores.pop();
                 }
+
+                if (!operadores.empty() &&
+                    operadores.top() == '(') {
+
+                    operadores.pop();
+                }
+            }
+            else if (actual == '+' ||
+                actual == '-' ||
+                actual == '*' ||
+                actual == '/') {
+
+                while (!operadores.empty() &&
+                    operadores.top() != '(' &&
+                    precedencia(operadores.top()) >= precedencia(actual)) {
+
+                    postfija += operadores.top();
+                    postfija += " ";
+
+                    operadores.pop();
+                }
+
                 operadores.push(actual);
             }
         }
     }
 
-    if (tempNum!=""){
-        postfija+=tempNum+ " ";
+    if (tempNum != "") {
+
+        postfija += tempNum + " ";
     }
 
     while (!operadores.empty()) {
+
         postfija += operadores.top();
         postfija += " ";
+
         operadores.pop();
     }
 
-    return postfija; 
+    return postfija;
 }
 
-int evaluarPostfija(string postfija){
-    Pila<int> valores; 
+int evaluarPostfija(string postfija) {
+
+    Pila<int> valores;
+
     string tempNum = "";
 
-    for(int i=0;i<postfija.length();i++){
-        char actual=postfija[i];
-        if(isdigit(actual)){
-            tempNum+=actual;
+    for (int i = 0; i < postfija.length(); i++) {
+
+        char actual = postfija[i];
+
+        if (isdigit(actual)) {
+
+            tempNum += actual;
         }
-        else if(actual==' '){ 
-            if(tempNum!=""){ // ya termino el numero
+
+        else if (actual == ' ') {
+
+            if (tempNum != "") {
+
                 valores.push(stoi(tempNum));
-                tempNum="";
+
+                tempNum = "";
             }
         }
-        else if (actual == '+' || actual == '-' || actual == '*' || actual == '/') {
-            int num2=valores.top();
+
+        else if (actual == '+' ||
+            actual == '-' ||
+            actual == '*' ||
+            actual == '/') {
+
+            int num2 = valores.top();
             valores.pop();
-            int num1=valores.top();
+
+            int num1 = valores.top();
             valores.pop();
-            int resultado=0;
+
+            int resultado = 0;
 
             if (actual == '+') {
                 resultado = num1 + num2;
-            } else if (actual == '-') {
+            }
+            else if (actual == '-') {
                 resultado = num1 - num2;
-            } else if (actual == '*') {
+            }
+            else if (actual == '*') {
                 resultado = num1 * num2;
-            } else if (actual == '/') {
-                resultado = num1 / num2; 
+            }
+            else if (actual == '/') {
+                resultado = num1 / num2;
             }
 
-            valores.push(resultado); 
+            valores.push(resultado);
         }
-        return valores.top();
     }
+
+    return valores.top();
 }
 
-
 int main() {
-    // Ecuación de prueba: 45 + (38 * 5) - 7 = 228
-    string expresionInfija = "45 + 38 * 5 - 7"; 
-    
-    cout << "1. Original: " << expresionInfija << endl;
+
+    string expresionInfija = "(3+5)*(7-4)";
+
+    cout << "Original: " << expresionInfija << endl;
 
     string expresionPostfija = infijaPostfija(expresionInfija);
-    cout << "2. Formato RPN (Postfija): " << expresionPostfija << endl;
+
+    cout << "Postfija: " << expresionPostfija << endl;
 
     int resultado = evaluarPostfija(expresionPostfija);
-    cout << "3. Resultado final: " << resultado << endl;
 
-    return 0;
+    cout << "Final: " << resultado << endl;
 }
